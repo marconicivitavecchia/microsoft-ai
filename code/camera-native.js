ab004662934d62fe41bdaed554403bec451d0dba
claudio <claudio.capobianco@istruzione.it>
2019-05-24 13:34:02 +0100

    remove camera-native support

---
+++ code/camera-native.js
@@ -0,0 +1,23 @@
+// This example uses https://github.com/sandeepmistry/node-raspberry-pi-camera-native
+// require module
+
+console.log("This example uses raspberry-pi-camera-native and it is not supported anymore");
+console.log("Please use pi-camera");
+return;
+const raspberryPiCamera = require('raspberry-pi-camera-native');
+
+// add frame data event listener
+raspberryPiCamera.on('frame', (frameData) => {
+	// frameData is a Node.js Buffer
+	console.log("frame!!");	
+});
+
+// start capture
+const cameraOptions = {
+	  width: 3280,
+	  height: 2464,
+	  fps: 1,
+	  encoding: 'JPEG',
+	  quality: 100
+}
+raspberryPiCamera.start(cameraOptions);
