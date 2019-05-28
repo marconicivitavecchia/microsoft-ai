afe423e585875a5dd1d8f97a6117f6ac2cb1219b
wbigger <wbigger@gmail.com>
2019-05-28 17:16:17 +0200

    Make emotion strength with gradient

--- code/hello-frog/src/app/emotion-strength/emotion-strength.component.ts
+++ code/hello-frog/src/app/emotion-strength/emotion-strength.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-emotion-strength',
   templateUrl: './emotion-strength.component.html',
   styleUrls: ['./emotion-strength.component.css']
 })
-export class EmotionStrengthComponent implements OnInit {
+export class EmotionStrengthComponent implements OnChanges {
 
-  @Input() value : number;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input() value: number;
+    valueWidth: number;
+    
+    ngOnChanges(): void {
+        this.valueWidth = this.value * 80;
+    }
 
 }
