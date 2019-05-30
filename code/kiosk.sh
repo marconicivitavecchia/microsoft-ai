ba438a27a45f6e338d30a52358bc22d7f26b28ef
claudio <claudio.capobianco@istruzione.it>
2019-05-30 05:31:35 +0100

    Add sleep to wait boot

--- code/kiosk.sh
+++ code/kiosk.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+# Wait some time that system boot up completely
+sleep 10
  
 # Run this script in display 0 - the monitor
 export DISPLAY=:0
  
 # Hide the mouse from the display
 unclutter &
+
+# Kill old session of chromium, node and http-server
+sudo killall chromium-browser node http-server
  
 # If Chrome crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
@@ -13,13 +19,13 @@ sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium
 # Run the back-end web server
 cd /home/pi/microsoft-ai/code/server/
 node main.js &
-sleep 3
+sleep 5
 
 # Run the front-end web server
 echo "Starting front-end web server..."
 cd /home/pi/hello-frog
 http-server . &
-sleep 2
+sleep 5
 
 # Run Chromium and open tabs
 echo "Starting chromium..."
